399d3a2b02a16340d97f4ec1443374282d87df13
Hallison Batista <email@hallisonbatista.com>
2010-01-22 23:58:50 -0400

    Updates in source documentation and manpages

--- lib/bash-toolbox/cli/bdoc.sh
+++ lib/bash-toolbox/cli/bdoc.sh
@@ -16,13 +16,30 @@
 #:
 #: Written by Hallison Batista &lt;hallison@codigorama.com$gt;
 #:
-#: Timestamp
-#: : 2010-01-08 17:59:17 -04:00
-#:
 #: ## COPYRIGHT
 #:
-#: Copyright (C) 2009-2010 Codigorama &lt;code@codigorama.com&gt;
+#: Copyright (C) 2009,2010 Codigorama &lt;code@codigorama.com&gt;
 #:
+#: Permission is hereby granted, free of charge, to any person obtaining a copy
+#: of this software and associated documentation files (the "Software"), to deal
+#: in the Software without restriction, including without limitation the rights
+#: to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#: copies of the Software, and to permit persons to whom the Software is
+#: furnished to do so, subject to the following conditions:
+#: 
+#: The above copyright notice and this permission notice shall be included in
+#: all copies or substantial portions of the Software.
+#: 
+#: THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#: IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#: FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#: AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#: LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#: OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+#: THE SOFTWARE.
+#:
+
+# Timestamp: 2010-01-08 17:59:17 -04:00
 
 # Extract only Markdown contents from the comments started by '#:'
 function srcdoc {
